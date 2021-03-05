21dedb795bc9c85ff33839bfb490821a7171f205
EgorZh1 <79202766+EgorZh1@users.noreply.github.com>
2021-03-05 20:35:53 +0300

    Add files via upload

---
+++ lab2(1)_11111111.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            string s = Console.ReadLine();
+
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == s.ToUpper()[i] && !(s[i] >= 32 && s[i] <= 127)) x++;
+            }
+            Console.WriteLine("Количество заглавных букв в строке, не входящих в английский алфавит: " + x);
+        }
+    }
+}
