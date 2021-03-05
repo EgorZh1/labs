ac2f4b6e8796bceeef6f96f1cbf83dffa50bfb3d
EgorZh1 <79202766+EgorZh1@users.noreply.github.com>
2021-03-05 18:33:03 +0300

    Add files via upload

---
+++ Lab2(1).cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2_1_
+{
+    class Program
+    {
+        static void Main()
+        {
+            DateTime date = DateTime.Now;
+            int[] s = new int[10];
+            string str1 = Convert.ToString(date), str2 = Convert.ToString(date.ToString("F"));
+            
+            for (int i = 0; i < str1.Length; i++) if (str1[i] >= 48 && str1[i] <= 57) s[str1[i] - 48]++;
+            
+            Console.WriteLine("Текущее время (1):  " + str1);
+            for (int i = 0; i < 10; i++) Console.WriteLine("Количество символов " + '\'' + i + "\' = " + s[i]);
+            for (int i = 0; i < 10; i++) s[i] = 0;
+
+            for (int i = 0; i < str2.Length; i++) if (str2[i] >= 48 && str2[i] <= 57) s[str2[i] - 48]++;
+            
+            Console.WriteLine("\n\n\nТекущее время (2):  " + str2);
+            for (int i = 0; i < 10; i++) Console.WriteLine("Количество символов " + '\'' + i + "\' = " + s[i]);
+            
+        }
+    }
+}
---
+++ Lab2(5).cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2_5_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            int count = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == str.ToUpper()[i] && !(str[i] >= 32 && str[i] <= 127)) count++;
+            }
+            Console.WriteLine("Количество заглавных букв в строке, не входящих в английский алфавит: " + count);
+        }
+    }
+}
