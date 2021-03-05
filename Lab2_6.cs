a62784a8b523b6282e6b5271e01a521925e62f6f
EgorZh1 <79202766+EgorZh1@users.noreply.github.com>
2021-03-05 18:54:29 +0300

    Add files via upload

---
+++ Lab2_6.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2__
+{
+    class Program
+    {
+        private static void Per(string str, int l, int r)
+        {
+            if (l == r)
+                Console.WriteLine(str);
+            else
+            {
+                for (int i = l; i <= r; i++)
+                {
+                    str = Swap(str, l, i);
+                    Per(str, l + 1, r);
+                    str = Swap(str, l, i);
+                }
+            }
+        }
+
+        public static string Swap(string str, int i, int j)
+        {
+            var chA = str.ToCharArray();
+            char temp = chA[i];
+            chA[i] = chA[j];
+            chA[j] = temp;
+            return new String(chA);
+        }
+
+        public static void Main()
+        {
+            string str;
+            str = Console.ReadLine();
+            Per(str, 0, str.Length-1);
+        }
+
+    }
+}
