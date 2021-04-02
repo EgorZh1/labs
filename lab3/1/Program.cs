c4103d566cf45c636f01d6556cb1220319a44fcf
EgorZh1 <79202766+EgorZh1@users.noreply.github.com>
2021-04-02 18:52:26 +0300

    Add files via upload

---
+++ lab3/1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3_1_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, sum=0, num, ranum;
+            
+            Console.Write("Введите количество чисел -");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Random rand = new Random();
+
+            Student a1 = new Student(n);
+            
+            for(int i=0; i<n; i++)
+            {
+                a1[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Количество символов - {0}", a1.leng);
+            
+            for (int i = 0; i < n; i++)
+            {
+                sum += a1[i];
+                s = a1.
+            }
+            Console.WriteLine("Сумма чисел - {0}", sum);
+            Console.WriteLine("Угадай число.");
+            ranum = rand.Next(1, sum + 1);
+          //  Console.WriteLine(ranum);
+            do
+            {
+               num = Convert.ToInt32(Console.ReadLine());
+               if (Student.Enter(num, ranum) == 1) break;
+            } while (true);
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ lab3/1/Student.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3_1_
+{
+    class Student
+    {
+        int[] a;
+        public int leng;
+        public Student(int size)
+        {
+            a = new int [size];
+            leng = size;
+        }
+        public int this[int index]
+        {
+            set { a[index] = value; }
+            get { return a[index]; }
+        }
+        static public int Enter(int num,int ranum)
+        {
+            if (num > ranum) Console.WriteLine("Загаданное число меньше!");
+            else if (num < ranum) Console.WriteLine("Загаданное число больше!");
+            else
+            {
+                Console.WriteLine("Ты угадал )");
+                return 1;
+            }
+            return 0;
+        }
+    }
+}
