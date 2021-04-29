c683703dbb30cd946a1ae4aa7c97fea8167776bd
EgorZh1 <79202766+EgorZh1@users.noreply.github.com>
2021-04-29 22:18:50 +0300

    Add files via upload

---
+++ Laba5/Laba5/Mathamatics.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba5
+{
+    public abstract class Math
+    {
+        public abstract int Schet();
+    }
+    public class M1: Math
+    {
+        public int length, width;
+        public M1(int a, int b)
+        {
+            length = a;
+            width = b;
+        }
+        public override int Schet()
+        {
+            Console.WriteLine("Вы не переопределили метод\n"+(length * width)+"\n");
+            return (length * width);
+        }
+    }
+     public class M2: Math
+     {
+         public int length, width;
+         public M2(int a, int b)
+         {
+             length = a*2;
+             width = b*2;
+         }
+         public override int Schet()
+         {
+             Console.WriteLine("Вы переопределили метод\n"+(length * width)+"\n");
+             return (length * width);
+         }
+
+     }
+}
---
+++ Laba5/Laba5/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba5
+{
+    class Program
+    {
+        enum Oper
+        {
+            One=1,
+            Two,
+            Three,
+            Four,
+            Five,
+            Six,
+            Seven,
+            Eight,
+            Nine,
+            Ten
+        }
+        static void Main(string[] args)
+        {
+            Oper s;
+
+            int l, w;
+
+            Console.Write("Введите длину l:= ");
+            l = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nВведите ширину w:= ");
+            w = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Перечесления");
+            s = Oper.One;
+            Console.WriteLine((int)s);
+            s = Oper.Ten;
+            Console.WriteLine((int)s);
+            
+            Console.WriteLine();
+            var Classes = new List<Math>
+            {
+                new M1(l,w),
+                new M2(l,w)
+            };
+            foreach (var i in Classes) i.Schet();
+          
+        }
+    }
+}
