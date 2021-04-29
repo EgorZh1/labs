ffce4a0ea0e7d4398656dd6a3197d5e0073bb9bc
EgorZh1 <79202766+EgorZh1@users.noreply.github.com>
2021-04-29 23:40:49 +0300

    Add files via upload

---
+++ Laba6/Laba6/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba6
+{
+    interface IComparable
+    {
+        void CompareTo();
+    }
+    public interface I_Fig
+    {
+        int G_P();
+        int G_A();
+    }
+    public class Class1 : I_Fig, IComparable
+    {
+        public int x,z;
+        public Class1(int a, int b)
+        {
+            x = a;
+            z = b;
+        }
+        public void CompareTo()
+        {
+            if (x > z) Console.WriteLine("X > Z");
+            if (z > x) Console.WriteLine("Z > X");
+        }
+        public int G_A()
+        {
+            return x*z;
+        }
+        public int G_P()
+        {
+            return 2*(x+z);
+        }
+    }
+}
---
+++ Laba6/Laba6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length, width;
+            Console.Write("Dlina: ");
+            length = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Shirina: ");
+            width = Convert.ToInt32(Console.ReadLine());
+
+            Class1 r = new Class1(length,width);
+            Console.WriteLine("Area is: {0}" ,r.G_A());
+            Console.WriteLine("Perimeter is: {0}\n" ,r.G_P());
+
+        }
+    }
+}
